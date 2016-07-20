f107e7ea17643a8a059e0fa514583d99a7363a17
gracefulife <gracefulife@naver.com>
2016-07-20 18:29:05 +0900

    2. 연산자 과제

---
+++ java/operator/src/com/openknowl/tutorials/Subject.java
@@ -0,0 +1,29 @@
+package com.openknowl.tutorials;
+
+public class Subject {
+    /*
+     * 에금 입출력 시나리오.
+     *
+     * 가상의 계좌 2개를 갖고,
+     * 각각 예금, 인출, 계좌이체를 순서대로 진행하는 소스코드를 작성.
+     * 각 순서마다 현재 계좌 상태를 출력할 것
+     *
+     */
+    public static void main(String[] args) {
+        int account1 = 1000000000;
+        int account2 = 1000000000;
+
+        // deposit
+        // 각각의 계좌에 10억 이하의 예금을 진행할 것
+        // 20 억을 넣어서 출력해보면 이상한 값이 나오는데 .. 왜 그런지 알아 올 것.
+
+        // withdraw
+        // 각각의 계좌에 예금액 이하의 인출을 진행할 것
+
+
+        // transfer
+        // 1번 계좌의 특정 금액을 2번 계좌에 옮길것. (뺴기 -> 더하기)
+
+    }
+
+}
