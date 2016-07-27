20255f2a7302ff62d91c6787fb3fab01f740baf7
gracefulife <gracefulife@naver.com>
2016-07-27 14:01:19 +0900

    fix typo

--- java/java_tutorial_4/throws/src/com/openknowl/tutorials/Subject.java
+++ java/java_tutorial_4/throws/src/com/openknowl/tutorials/Subject.java
@@ -14,7 +14,7 @@ public class Subject {
          * 4를 입력하면 조회를 할 수 있는 메뉴를 콘솔에 작성.
          * 5를 입력하면 종료. (종료는 return 문을 이용하지 않고 시킬 것)
          * 종료시에는 모든 계좌의 예금 상태를 보여줄 것
-         * java_tutorial_3/subject2 의 과제 제출한것을 기반으로 작업
+         * java_tutorial_4/subject1 의 과제 제출한것을 기반으로 작업
          *
          * throws (https://opentutorials.org/course/1223/6227) 를 통해 학습하고
          * 예외가 발생했을 때 안내메세지와 함께 다시 메뉴를 입력받도록 플로우를 조정한다.
